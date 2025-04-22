dbdc8edff31113bca365bd5fbe1351caab499172
Yusif Y. Qabil <yusifqabil@outlook.com>
2025-04-22 22:26:48 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# IMDb-top-250-movies-UsingSelenium
+# IMDb-top-250-movies
+A Web Scraper that uses selenium to scrape data about the top 250 movies from IMDb
---
+++ main.py
@@ -0,0 +1,19 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+import pandas as pd
+driver = webdriver.Chrome()
+result = []
+driver.get("https://www.imdb.com/chart/top/")
+allMovies = driver.find_elements(By.CLASS_NAME,"ipc-metadata-list-summary-item")
+for movie in allMovies : 
+    name = movie.find_element(By.TAG_NAME,"h3").text
+    rating = movie.find_element(By.CLASS_NAME,"ipc-rating-star--rating").text 
+    allSpans = movie.find_elements(By.TAG_NAME,"span")
+    year = allSpans[1].text
+    duration = allSpans[2].text
+    print(year)
+    result.append((name,year,duration,rating))
+df = pd.DataFrame(result,columns=["name","year","duration","rating"])
+with open("output.csv","w") as out : 
+    df.to_csv(out,index=False)
+driver.quit()
